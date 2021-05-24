33be699c960844a8cfb633a4ddbf2eca7da056c1
RoozbehBandpey <r.bandpey67@gmail.com>
2021-05-24 18:00:34 +0200

    passing params

--- azure-k8-service/infrastructure/deployment-scripts/check-keyvault.sh
+++ azure-k8-service/infrastructure/deployment-scripts/check-keyvault.sh
@@ -1,10 +1,9 @@
 # get Command Line Args
 baseName=$1
-environment=$2
 resourceGroupName=$3
 
 # Check if KeyVault exists
-az keyvault show -n "$baseName-kv-$environment" -g $resourceGroupName
+az keyvault show -n "$baseName" -g $resourceGroupName
 # Use recover mode keep ML ws's access policies
 if [ $? -eq 0 ]; then
     echo "keyvault recovery deployment"
