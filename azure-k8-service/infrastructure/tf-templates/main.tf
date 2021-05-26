b59fdbc8945315607a36ccea8f2d1b3e25d2993b
RoozbehBandpey <Roozbeh.Bandpey@partner.bmw.com>
2021-05-26 10:31:07 +0200

    0.14.8

--- azure-k8-service/infrastructure/tf-templates/main.tf
+++ azure-k8-service/infrastructure/tf-templates/main.tf
@@ -1,11 +1,12 @@
-# terraform {
+terraform {
+	required_version = "= 0.14.8"
 #   required_providers {
 #     azurerm = {
 #       source  = "hashicorp/azurerm"
 #       version = ">= 2.26"
 #     }
 #   }
-# }
+}
 
 provider "azurerm" {
  features {}
