d71fdf3fb19169489e3ad0b7676f9ae974e7cadc
RoozbehBandpey <r.bandpey67@gmail.com>
2021-05-28 15:13:55 +0200

    list-deleted

--- azure-k8-service/infrastructure/deployment-scripts/check-keyvault.sh
+++ azure-k8-service/infrastructure/deployment-scripts/check-keyvault.sh
@@ -3,7 +3,8 @@ baseName=$1
 environment=$2
 
 # Check if KeyVault exists
-az keyvault show -n "$baseName-kv-$environment"
+# az keyvault show -n "$baseName-kv-$environment"
+az keyvault list-deleted --query "contains([].name, '$baseName-kv-$environment')"
 # Use recover mode keep ML ws's access policies
 if [ $? -eq 0 ]; then
     echo "keyvault recovery deployment"
