c468f50df14e16dbe495498759a9fce2138359ab
RoozbehBandpey <r.bandpey67@gmail.com>
2021-05-28 15:32:42 +0200

    bash debugged

--- azure-k8-service/infrastructure/deployment-scripts/check-keyvault.sh
+++ azure-k8-service/infrastructure/deployment-scripts/check-keyvault.sh
@@ -4,12 +4,12 @@ environment=$2
 
 # Check if KeyVault exists
 # az keyvault show -n "$baseName-kv-$environment"
-az keyvault list-deleted --query "contains([].name, '$baseName-kv-$environment')"
+deleted=$(az keyvault list-deleted --query "contains([].name, '$baseName-kv-$environment')")
 # Use recover mode keep ML ws's access policies
-if [ $? -eq 0 ]; then
-    echo "keyvault recovery deployment"
+if [ $deleted == true ]; then
+    echo "keyvault '$baseName-kv-$environment' was soft deleted! recovery deployment"
     echo "##vso[task.setvariable variable=createmode]recover"
 else
-    echo "keyvault default deployment"
+    echo "keyvault '$baseName-kv-$environment' was not soft deleted! default deployment"
     echo "##vso[task.setvariable variable=createmode]default"
 fi
